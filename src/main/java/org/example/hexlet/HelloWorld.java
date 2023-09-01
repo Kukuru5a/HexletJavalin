f9736d0b8047d6db6166913bf0194e4d63662e59
kukuru5a <romanzag@icloud.com>
2023-09-01 21:15:27 +0600

    added to localhost hello command & hello + {name} command

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -5,7 +5,11 @@ import io.javalin.Javalin;
 public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(config -> {config.plugins.enableDevLogging();});
-        app.get("/",ctx -> ctx.result("Hello, World!"));
+        app.get("/hello/{name}",ctx -> {
+//            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("Hello, World!");
+            ctx.result("Hello, " + ctx.pathParam("name" + "!"));
+        });
+        app.get("/hello", ctx -> ctx.result("Hello, World!"));
         app.start(7070);
     }
 }
